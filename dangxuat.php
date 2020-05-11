ce75b3fa75a9bff547769829e87416d3f58b94ec
ninhhuynh1999 <44170840+ninhhuynh1999@users.noreply.github.com>
2020-05-11 09:18:47 +0700

    Delete dangxuat.php

--- dangxuat.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if(isset($_POST['dangxuat'])){
-        setcookie("siteAuth", "", time() - 3600);
-        header('location:index.php');//chuyền qua trang index neu out thanh cong
-    }
-?>
